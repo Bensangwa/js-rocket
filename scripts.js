7eab6dc1c6ab5979668ea76166606fef352f32a5
bensang <bensang>
2018-02-13 09:57:36 +0000

    removed JS element

--- scripts.js
+++ scripts.js
@@ -4,10 +4,7 @@ $(document).ready(function(){
         $(".call-to-action").slideToggle();
     });
 
-    //$(".call-button").on("click", function(e){
-    //    e.preventDefault();
-    //    $(".column img").toggleClass("bigger");
-   // });
+
 
     
 
